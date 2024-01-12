15f558f6354d96398ce6f9b0de9863fab33177ca
Pedro Fernandez <pedrolufernandez92@gmail.com>
2024-01-12 12:35:00 +0100

    Objetos Allergen y Menu creados

---
+++ js/Allergen.js
@@ -0,0 +1,27 @@
+class Allergen {
+  constructor(name) {
+    this.name = name;
+    this.description = "";
+  }
+}
+
+Object.defineProperty(this, "fullName", {
+  enumerable: true,
+  get() {
+    return this._name;
+  },
+  set(name) {
+    if (typeof name !== "string") throw new Error("Name must be a string");
+    else this.name = name;
+  },
+});
+
+Object.defineProperty(this, "description", {
+  enumerable: true,
+  get() {
+    return this.description;
+  },
+  set(description) {
+    this.description = description;
+  },
+});
